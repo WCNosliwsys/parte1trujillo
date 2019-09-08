750e30de84b1e15ba7ddbed767ff8234df579d96
Wilson Callisaya <demarktec@gmail.com>
2019-09-08 09:59:49 -0500

    Actividad1 finalizado

--- app/src/main/java/com/ingwilson/primertaller/MiFragmentTabHost.java
+++ app/src/main/java/com/ingwilson/primertaller/MiFragmentTabHost.java
@@ -6,20 +6,21 @@ import androidx.fragment.app.FragmentTabHost;
 
 import android.os.Bundle;
 
-public class MiFragmentTabHost  extends FragmentActivity {
+public class MiFragmentTabHost extends FragmentActivity {
     private FragmentTabHost tabHost;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_mi_fragment_tab_host);
         tabHost = (FragmentTabHost) findViewById(android.R.id.tabhost);
         tabHost.setup(this,
-                getSupportFragmentManager(),android.R.id.tabcontent);
+                getSupportFragmentManager(), android.R.id.tabcontent);
         tabHost.addTab(tabHost.newTabSpec("tab1").setIndicator("Lengüeta 1"),
                 Tab1.class, null);
-/* tabHost.addTab(tabHost.newTabSpec("tab2").setIndicator("Lengüeta 2"),
-Tab2.class, null);
-tabHost.addTab(tabHost.newTabSpec("tab3").setIndicator("Lengüeta 3"),
-Tab3.class, null);*/
+        tabHost.addTab(tabHost.newTabSpec("tab2").setIndicator("Lengüeta 2"),
+                Tab2.class, null);
+        tabHost.addTab(tabHost.newTabSpec("tab3").setIndicator("Lengüeta 3"),
+                Tab3.class, null);
     }
 }
